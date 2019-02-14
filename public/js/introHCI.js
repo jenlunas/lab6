92e2e2d297d0bb7c0651a65daaa94c397811ea76
Jennifer Sandoval <jennifersandoval94@gmail.com>
2019-02-14 10:48:12 -0800

    Added facebook login

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,15 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("http://localhost:3000/project/" + idNumber, addProject);
+	$('#' + idNumber)
+}
+
+function addProject(result){
+	 var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
 }
